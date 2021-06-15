b41c05e150edde4bbc77671ecbaec01ed2937792
rregalado17 <rregalado17@gmail.com>
2021-06-14 21:00:48 -0400

    create wallet controllers

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,2 +1,20 @@
 class WalletsController < ApplicationController
+    def index
+        wallets = Wallets.all 
+        render json: wallet
+    end
+
+    def show 
+        User = User.find(params[:id])
+        render json: wallet
+    end
+
+    def create 
+        @user = User.find_by_id(session[:admin_id])
+        @wallet = Wallet.new(wallet_params.merger(user_id: current_user.id))
+        if @wallet.save
+            redirect_to wallet_path(@wallet)
+        else
+            render :new
+    end
 end
